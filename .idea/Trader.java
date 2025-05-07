79b4cd15e3819248eb68bef275c68005ee9d70aa
Armine <91663103+GrigoryanArmine@users.noreply.github.com>
2025-05-07 21:03:55 +0400

    Update Trader.java

--- .idea/Trader.java
+++ .idea/Trader.java
@@ -1,15 +1,16 @@
+// Trader.java
 import java.util.ArrayList;
 import java.util.List;
 
 public class Trader extends User {
     private double balance;
-    //private Portfolio portfolio;
-    private List<Transaction> transactionHistory;
+    private final Portfolio portfolio;
+    private final List<Transaction> transactionHistory;
 
     public Trader(String name, double initialBalance) {
         super(name, UserType.TRADER);
         this.balance = initialBalance;
-        //this.portfolio = new Portfolio();
+        this.portfolio = new Portfolio();
         this.transactionHistory = new ArrayList<>();
     }
 
@@ -17,50 +18,59 @@ public class Trader extends User {
         return balance;
     }
 
-    //public Portfolio getPortfolio() {
+    public Portfolio getPortfolio() {
         return portfolio;
     }
 
     public List<Transaction> getTransactionHistory() {
-        return transactionHistory;
+        return new ArrayList<>(transactionHistory); // Return copy for encapsulation
     }
 
     public void buyStock(Stock stock, int quantity, double pricePerUnit) throws InsufficientFundsException {
         double totalCost = quantity * pricePerUnit;
         if (totalCost > balance) {
-            // throw new InsufficientFundsException("Not enough funds to buy " + quantity + " shares of " + stock.getSymbol());
+            throw new InsufficientFundsException(
+                String.format("Insufficient funds: %.2f needed, but balance is %.2f", 
+                              totalCost, balance));
         }
+        
         balance -= totalCost;
-        // portfolio.addStock(stock.getSymbol(), quantity);
-        Transaction transaction = new Transaction(name, stock.getSymbol(), quantity, pricePerUnit);
-        transactionHistory.add(transaction);
+        portfolio.addStock(stock.getSymbol(), quantity);
+        transactionHistory.add(new Transaction(
+            this.getUserId(), 
+            stock, 
+            quantity, 
+            pricePerUnit
+        ));
     }
+
     public void sellStock(Stock stock, int quantity) throws InvalidQuantityException {
-        if (quantity <= 0) throw new InvalidQuantityException(quantity);
-
-        int owned = 0;
-        for (Stock s : portfolio) {
-            if (s.getSymbol().equals(stock.getSymbol())) owned++;
+        if (quantity <= 0) {
+            throw new InvalidQuantityException("Invalid quantity: " + quantity);
         }
-        if (owned < quantity) throw new InvalidQuantityException("Not enough shares to sell");
 
-        int removed = 0;
-        for (int i = 0; i < portfolio.size() && removed < quantity; i++) {
-            if (portfolio.get(i).getSymbol().equals(stock.getSymbol())) {
-                portfolio.remove(i);
-                removed++;
-                i--;
-            }
-        }
-        balance += stock.getCurrentPrice() * quantity;
+        portfolio.removeStock(stock.getSymbol(), quantity);
+        double totalRevenue = quantity * stock.getCurrentPrice();
+        balance += totalRevenue;
+        transactionHistory.add(new Transaction(
+            this.getUserId(), 
+            stock, 
+            -quantity, 
+            stock.getCurrentPrice()
+        ));
+    }
+
+    public void viewPortfolio() {
+        portfolio.display();
+    }
+
+    @Override
+    public String getRoleDetails() {
+        return "Professional stock trader with portfolio management capabilities";
     }
 
     @Override
     public String toString() {
-        return super.toString() + "\nBalance: " + balance;
+        return super.toString() + "\nBalance: $%.2f", balance;
     }
-
-    // public void viewPortfolio() {
-    //     portfolio.display();
-    // }
 }
