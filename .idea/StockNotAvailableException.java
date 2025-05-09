07c6af08fdefd7c13a19fedab1d7e9eea4390c12
Harut <allahverdyanharut21@gmail.com>
2025-05-09 12:08:18 +0400

    Create StockNotAvailableException.java

---
+++ .idea/StockNotAvailableException.java
@@ -0,0 +1,10 @@
+package exceptions;
+public class StockNotAvailableException extends RuntimeException {
+    public StockNotAvailableException(String symbol) {
+        super("Stock not available: " + symbol);
+    }
+
+    public StockNotAvailableException() {
+        super("Stock not available");
+    }
+}
