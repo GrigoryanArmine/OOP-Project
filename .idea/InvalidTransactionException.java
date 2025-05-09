0c4e3571faf973b1ebcf0ba4bc76d57b982abae0
Harut <allahverdyanharut21@gmail.com>
2025-05-09 12:09:44 +0400

    Update InvalidTransactionException.java

--- .idea/InvalidTransactionException.java
+++ .idea/InvalidTransactionException.java
@@ -1,5 +1,15 @@
-public class InvalidTransactionException extends Exception {
-    public InvalidTransactionException(String message) {
+package exceptions;
+
+public class InvalidQuantityException extends RuntimeException {
+    public InvalidQuantityException(double quantity) {
+        super("Invalid quantity: " + quantity);
+    }
+
+    public InvalidQuantityException(String message) {
         super(message);
+    }
+
+    public InvalidQuantityException() {
+        this("Quantity must be a positive number");
     }
 }
