b1a04d3c941fa663b0c0709f2fc1c33b56c92ae3
Armine <91663103+GrigoryanArmine@users.noreply.github.com>
2025-05-09 13:03:42 +0400

    Update Trader.java

--- src/main/java/models/Trader.java
+++ src/main/java/models/Trader.java
@@ -3,47 +3,71 @@ package models;
 import exceptions.*;
 import java.util.ArrayList;
 
-public class Trader {
-    private String name;
+public class Trader extends User {
     private double balance;
     private ArrayList<Stock> portfolio;
+    private final TransactionHistory transactionHistory;
 
-    public Trader(String name, double balance) {
-        this.name = name;
-        this.balance = balance;
+    public Trader(String name, double initialBalance) {
+        super(name, UserType.TRADER);
+        this.balance = initialBalance;
         this.portfolio = new ArrayList<>();
+        this.transactionHistory = new ArrayList<>();
     }
 
-    public void buyStock(Stock stock, int quantity) throws InsufficientFundsException, InvalidQuantityException {
-        if (quantity <= 0) throw new InvalidQuantityException(quantity);
-        double totalCost = stock.getCurrentPrice() * quantity;
-        if (balance < totalCost) throw new InsufficientFundsException(balance, totalCost);
-
-        balance -= totalCost;
-        for (int i = 0; i < quantity; i++) {
-            portfolio.add(stock);
-        }
+    public double getBalance() {
+        return balance;
     }
-    public void sellStock(Stock stock, int quantity) throws InvalidQuantityException {
-        if (quantity <= 0) throw new InvalidQuantityException(quantity);
-
-        int owned = 0;
-        for (Stock s : portfolio) {
-            if (s.getSymbol().equals(stock.getSymbol())) owned++;
-        }
-        if (owned < quantity) throw new InvalidQuantityException("Not enough shares to sell");
-
-        int removed = 0;
-        for (int i = 0; i < portfolio.size() && removed < quantity; i++) {
-            if (portfolio.get(i).getSymbol().equals(stock.getSymbol())) {
-                portfolio.remove(i);
-                removed++;
-                i--;
-            }
-        }
-        balance += stock.getCurrentPrice() * quantity;
-    }
-
-    public double getBalance() { return balance; }
     public ArrayList<Stock> getPortfolio() { return portfolio; }
+
+    public List<Transaction> getTransactionHistory() {
+        return transactionHistory.getTransactionsByTrader(this.getName());
+    }
+
+    public void buyStock(Stock stock, int quantity, double pricePerUnit) throws InsufficientFundsException {
+        double totalCost = quantity * pricePerUnit;
+        if (totalCost > balance) {
+            throw new InsufficientFundsException(
+                String.format("Insufficient funds: %.2f needed, but balance is %.2f", 
+                              totalCost, balance));
+        }
+        
+        balance -= totalCost;
+        portfolio.addStock(stock.getSymbol(), quantity);
+        transactionHistory.addTransaction(new Transaction(
+            this.getName(),           
+            stock.getSymbol(),        
+            quantity,                 
+            pricePerUnit,             
+            true                    
+        ));
+    }
+
+    public void sellStock(Stock stock, int quantity) throws InvalidQuantityException {
+        if (quantity <= 0) {
+            throw new InvalidQuantityException("Invalid quantity: " + quantity);
+        }
+
+        portfolio.removeStock(stock.getSymbol(), quantity);
+        double totalRevenue = quantity * stock.getCurrentPrice();
+        balance += totalRevenue;
+        transactionHistory.addTransaction(new Transaction(
+            this.getName(),          
+            stock.getSymbol(),        
+            quantity,                
+            stock.getCurrentPrice(), 
+            false                   
+        ));
+    }
+    
+    @Override
+    public String getRoleDetails() {
+        return "Professional stock trader with portfolio management capabilities";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("\nBalance: $%.2f", balance);
+    }
+
 }
