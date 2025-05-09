ec8e504f438563e49d6677e859da74304e0aa839
Harut <allahverdyanharut21@gmail.com>
2025-05-09 12:09:11 +0400

    Update InsufficientFundsException.java

--- .idea/InsufficientFundsException.java
+++ .idea/InsufficientFundsException.java
@@ -1,5 +1,15 @@
-public class InsufficientFundsException extends Extension {
-  public InsufficientFundsException(String message) {
-    super(message);
-  }
+package exceptions;
+
+public class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(double balance, double required) {
+        super(String.format("Insufficient funds. Balance: $%.2f, Required: $%.2f", balance, required));
+    }
+
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+    public InsufficientFundsException() {
+        this("Insufficient funds for this transaction");
+    }
 }
