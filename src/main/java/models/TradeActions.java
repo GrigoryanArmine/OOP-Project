8615bd4074eaf03a79200ad7d511c12595f0351c
Harut <allahverdyanharut21@gmail.com>
2025-05-10 10:58:05 +0400

    Delete src/main/java/models/TradeActions.java

--- src/main/java/models/TradeActions.java
+++
@@ -1,9 +0,0 @@
-package main.models;
-
-import main.exceptions.*;
-
-public interface TradeActions {
-    void buyStock(Stock stock, int quantity) throws InsufficientFundsException;
-    void sellStock(Stock stock, int quantity) throws InvalidQuantityException, StockNotAvailableException;
-    void viewPortfolio();
-}
